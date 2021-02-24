7322ddf77169feff77e6b437c1b2f06cc952ab0b
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2021-02-24 10:37:25 +0100

    IMPROVE long_string_tests

--- trustinsoft/regenerate.py
+++ trustinsoft/regenerate.py
@@ -283,15 +283,182 @@ def make_librhash_test(librhash_test, machdep):
         "include_": path.join("trustinsoft", "%s.config" % machdep["machdep"])
     }
 
-tis_test_long_strings_cases = list(range(0, 32))
+long_strings_tests = [
+    { 
+        "hash_id": "CRC32",
+        "expected_hash": "467ED497"
+    },
+    { 
+        "hash_id": "CRC32C",
+        "expected_hash": "3128C6CB"
+    },
+    { 
+        "hash_id": "MD4",
+        "expected_hash": "9C88157A6F588E9815A9E6B60877D93E"
+    },
+    { 
+        "hash_id": "MD5",
+        "expected_hash": "0D0C9C4DB6953FEE9E03F528CAFD7D3E"
+    },
+    { 
+        "hash_id": "SHA1",
+        "expected_hash": "A080CBDA64850ABB7B7F67EE875BA068074FF6FE"
+    },
+    { 
+        "hash_id": "ED2K",
+        "expected_hash": "9C88157A6F588E9815A9E6B60877D93E"
+    },
+    { 
+        "hash_id": "AICH",
+        "expected_hash": "UCAMXWTEQUFLW637M7XIOW5ANADU75X6"
+    },
+    { 
+        "hash_id": "TIGER",
+        "expected_hash": "64EE4377C6BBE3A4C1963B6377CD68095F323F0E8E31ED22"
+    },
+    { 
+        "hash_id": "TTH",
+        "expected_hash": "BGOMMS6M6WVMYKELCO5BDMY4SS4SVCOA5X6DWGI"
+    },
+    { 
+        "hash_id": "WHIRLPOOL",
+        "expected_hash": "46639D6913ABF0FDBF31811EB66D5B86E1D369DD66C9A6DA81BE9B553A647A2F117EDB97FC237A23067043C7F68C2D8CC71B210862C1C716846AD11CD3FCBD98"
+    },
+    { 
+        "hash_id": "RIPEMD160",
+        "expected_hash": "EB33E86B2400CC0A11707BE717A35A9ACF074A58"
+    },
+    { 
+        "hash_id": "GOST94_CRYPTOPRO",
+        "expected_hash": "990DDD66E568294EE27A0C1E75BEF8E129C82832F7BABFDB3DB9D5210FA63752"
+    },
+    { 
+        "hash_id": "GOST94",
+        "expected_hash": "157782019557056EC7F756C26C5B6064048194C2E3FAB6A898DC4C3B882D0370"
+    },
+    { 
+        "hash_id": "HAS160",
+        "expected_hash": "17DB25293425CEA5FC78539654264B06A9FB2FB4"
+    },
+    { 
+        "hash_id": "SNEFRU128",
+        "expected_hash": "6256051A29F8D69D662169585670BB9A"
+    },
+    { 
+        "hash_id": "SNEFRU256",
+        "expected_hash": "407FE84009C371702FE9C4C0436AA271A62B702E96111F5747DC03EA18FBDBA6"
+    },
+    { 
+        "hash_id": "SHA224",
+        "expected_hash": "00568FBA93E8718C2F7DCD82FA94501D59BB1BBCBA2C7DC2BA5882DB"
+    },
+    { 
+        "hash_id": "SHA256",
+        "expected_hash": "27DD1F61B867B6A0F6E9D8A41C43231DE52107E53AE424DE8F847B821DB4B711"
+    },
+    { 
+        "hash_id": "SHA384",
+        "expected_hash": "2BCA3B131BB7E922BCD1DE98C44786D32E6B6B2993E69C4987EDF9DD49711EB501F0E98AD248D839F6BF9E116E25A97C"
+    },
+    { 
+        "hash_id": "SHA512",
+        "expected_hash": "0593036F4F479D2EB8078CA26B1D59321A86BDFCB04CB40043694F1EB0301B8ACD20B936DB3C916EBCC1B609400FFCF3FA8D569D7E39293855668645094BAF0E"
+    },
+    { 
+        "hash_id": "SHA3_224",
+        "expected_hash": "DEC1633E9A1F4BB6B2DBE04C38E4B43BA4C865165031E6A11E3EA389"
+    },
+    { 
+        "hash_id": "SHA3_256",
+        "expected_hash": "B61166B03A22F4ACE84E2AD281780AABBF3EB436CC71D281CEF9D8CAE1E1236D"
+    },
+    { 
+        "hash_id": "SHA3_384",
+        "expected_hash": "3323B42EF9C5B7321C47A27DD9C322F2AEF86EEEECB80D0344E51B29FCC5098D3C406CA94075BFD0DB8D062058724507"
+    },
+    { 
+        "hash_id": "SHA3_512",
+        "expected_hash": "E1F5A8A3D74990804F4E01FDB67D99E578C624D45C3DC2AA4EB1B9932644AF685E711708751FCC4E19FE9FA790572E2C328309AC3C17E97DEDF5AFCEBCFE118E"
+    },
+    { 
+        "hash_id": "EDONR256",
+        "expected_hash": "7CEFFD4222364F22AC6894386E039776E809B663990AB5098DC8086FCE8C4775"
+    },
+    { 
+        "hash_id": "EDONR512",
+        "expected_hash": "38D648FCB9F9146B235A68090B4A5A1250457B9B1296CA879FC41FC9621872F42AF951ADDA34895379CF9183E2141D0BABB70BFB2F1A44F332800F9E506B6A9C"
+    },
+    { 
+        "hash_id": "GOST12_256",
+        "expected_hash": "96054632C841E681072F3585F5535A3DB8C4AB73097FD1373B79943EC7244E18"
+    },
+    { 
+        "hash_id": "GOST12_512",
+        "expected_hash": "A3AD7AB0F63BCA7D38E5C34718836A0AEE3137DFCE6038F318B6B3C212537C84CE5F8940424C4BCC10A46F3C12D1CFCE6402E3879FDBE03FDFC4A711E8894634"
+    },
+    { 
+        "hash_id": "BTIH",
+        "expected_hash": "D6C874BC44C9283DCD2EA64EC1F9BFC7B530EE20",
+        "set_filename": True
+    },
+    {
+        "hash_id": "BTIH",
+        "expected_hash": "B99731F317F9FB4B5FA24D616B1EF5B59A063C1C"
+    },
+    {
+        "hash_id": "GOST94",
+        "ch": "0xFF",
+        "msg_size": 64,
+        "expected_hash": "13416C4EC74A63C3EC90CB1748FD462C7572C6C6B41844E48CC1184D1E916098"
+    },
+    {
+        "hash_id": "GOST94_CRYPTOPRO",
+        "ch": "0xFF",
+        "msg_size": 64,
+        "expected_hash": "58504D26B3677E756BA3F4A9FD2F14B3BA5457066A4AA1D700659B90DCDDD3C6"
+    }
+]
+
+# int set_filename = (tests[count].hash_id == RHASH_BTIH);
+# assert_rep_hash(tests[count].hash_id, 'a', 10000, tests[count].expected_hash, set_filename);
 
 def make_tis_test_long_strings(case_no, machdep):
     test_hashes_C_path = path.join("librhash", "test_hashes.c")
     test = make_librhash_test("test_long_strings", machdep)
     test["name"] = "%s : %s : CASE %d, %s" % (test_hashes_C_path, "test_long_strings", case_no, machdep["pretty_name"])
     test["main"] = "tis_test_long_strings"
-    test["cpp-extra-args"] = [ "-DTEST_CASE=%d" % case_no ]
+    # macros
+    case = long_strings_tests[case_no]
+    hash_id = "RHASH_" + case["hash_id"]
+    hash_name = "\\\"" + case["hash_id"] + "\\\""
+    if "set_filename" in case and case["set_filename"]:
+        set_filename = 1
+    else:
+        set_filename = 0
+    if "ch" in case:
+        ch = case["ch"];
+    else:
+        ch = "%d" % ord('a');
+    if "msg_size" in case:
+        msg_size = case["msg_size"];
+    else:
+        msg_size = 10000;
+    expected_hash = "\\\"" + case["expected_hash"] + "\\\""
+    test["cpp-extra-args"] = [
+        "-DTEST_LONG_STRINGS",
+        "-DCASE_NO=%d" % case_no,
+        "-DHASH_ID=%s" % hash_id,
+        "-DHASH_NAME=%s" % hash_name,
+        "-DSET_FILENAME=%d" % set_filename,
+        "-DCH=%s" % ch,
+        "-DMSG_SIZE=%d" % msg_size,
+        "-DEXPECTED_HASH=%s" % expected_hash,
+    ]
+
     return test
+
+
+
 
 def make_tis_config():
     return (
@@ -305,7 +472,7 @@ def make_tis_config():
         )) + 
         list(map(
             lambda t: make_tis_test_long_strings(t[0], t[1]),
-            product(tis_test_long_strings_cases, machdeps)
+            product(list(range(0, len(long_strings_tests))), machdeps)
         ))
     )
 
